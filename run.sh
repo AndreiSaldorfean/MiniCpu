e113979b23c2af7c42dc3d4f957142ff2e082ba9
Andrei Saldorfean <andreisaldorfean@gmail.com>
2025-01-06 15:26:36 +0200

    Update build script

--- run.sh
+++ run.sh
@@ -2,10 +2,11 @@ if [ "$1" = "c" ]; then
     if [ ! -d build ]; then
         mkdir build
     fi
-    ghdl -a src/alu.vhd
-    ghdl -a src/alu_tb.vhd
-    ghdl -e alu_entity_tb
-    ghdl -r alu_entity_tb --vcd=build/alu_tb.vcd
+    ghdl -a src/cpu.vhd
+    ghdl -a src/cpu_tb.vhd
+    ghdl -e cpu_entity_tb
+    ghdl -r cpu_entity_tb --vcd=build/cpu_tb.vcd
+    mv *.cf build
 else
-    gtkwave build/alu_tb.vcd
+    gtkwave build/cpu_tb.vcd
 fi
