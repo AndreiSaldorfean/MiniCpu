67583a4640e277c06f91f1d83adac9d7c897661e
Andrei Saldorfean <andreisaldorfean@gmail.com>
2025-01-08 23:59:25 +0200

    finnish block diagram and add code for new comps

--- src/cpu.vhd
+++ src/cpu.vhd
@@ -5,14 +5,22 @@ use ieee.numeric_std.all;
 entity cpu_entity is Port(
     Input  : in std_logic_vector(7 downto 0); --instruction word
     r_w    : in std_logic; --read/!write
-    debug  : in std_logic; --debug mode
-    step   : in std_logic; --step into memory location
-    count  : in std_logic; --count upwards/downwards
-    output : out unsigned(4 downto 0) --instruction output
+    load    : in std_logic;
+    cnt_down: in std_logic;
+    cnt_up  : in std_logic;
+    debug  : in std_logic;
+    step    : in std_logic;
+    output : out unsigned(3 downto 0)
 );
 end cpu_entity;
 
 architecture cpu_arch of cpu_entity is
+
+  -- Accumulator register
+  signal cp             : std_logic;
+  signal acc_data_in    : unsigned(4 downto 0);
+  signal acc_data_out   : unsigned(4 downto 0);
+  -- ALU
   type alu_action_t is
   (
     ALU_OP_ADD,
@@ -29,18 +37,26 @@ architecture cpu_arch of cpu_entity is
     ALU_ACCUMULATOR
   );
 
-  signal operand    : unsigned(2 downto 0);
-  signal a          : unsigned(4 downto 0);
-  signal imm        : unsigned(4 downto 0);
   signal alu_left   : unsigned(4 downto 0);
   signal alu_right  : unsigned(4 downto 0);
   signal alu_action : alu_action_t;
---================================================
--- 2K x 8 Static RAM
+  -- Decoder
+  signal do : unsigned(7 downto 0);
+  signal di : unsigned(2 downto 0);
+  -- PC
+  signal sel : std_logic_vector(1 downto 0);
+  signal pc_clk : std_logic;
+  signal q : unsigned( 7 downto 0);
+
+  signal internal_clk : std_logic;
+  signal reset : std_logic;
+  -- RAM 2K x 8 Static RAM
   signal instruction_mode : unsigned(7 downto 0);
   signal pc: unsigned(7 downto 0);
   signal mem_addr : unsigned(10 downto 0);
   signal mem_data : unsigned(7 downto 0);
+  signal operand        : unsigned(2 downto 0);
+  signal imm            : unsigned(4 downto 0);
 
 begin
 
@@ -55,27 +71,69 @@ begin
             mem_data<=Input;
           end if;
         end if;
-      end if;
 
   end process ram;
 
-  program_counter : process(clk,debug,step,count)
-    begin
-      if rising_edge(clk) then
-        if debug = '1' then
-            if step = '1' then
-                if count = '1' then
-                    pc <= pc + 1;
-                elsif count = '0' then
-                    pc <= pc - 1;
-                end if;
-            end if;
-        end if;
-    end if;
-  end process program_counter;
+  encoder: process(cnt_down, cnt_up, load, reset)
+  begin
+    if load = '0' then
+      if    cnt_down = '0' and cnt_up = '0' and reset = '0' then
+        sel <= "00";
+      elsif cnt_down = '0' and cnt_up = '0' and reset = '1' then
+        sel <= "00";
+        reset <= '0';
+      elsif cnt_down = '0' and cnt_up = '1' and reset = '0' then
+        sel <= "11";
+      elsif cnt_down = '0' and cnt_up = '1' and reset = '1' then
+        sel <= "00";
+        reset <= '0';
+      elsif cnt_down = '1' and cnt_up = '0' and reset = '0' then
+        sel <= "01";
+      elsif cnt_down = '1' and cnt_up = '0' and reset = '1' then
+        sel <= "00";
+        reset <= '0';
+      elsif cnt_down = '1' and cnt_up = '1' and reset = '0' then
+        sel <= "00";
+      elsif cnt_down = '1' and cnt_up = '1' and reset = '1' then
+        sel <= "00";
+        reset <= '0';
+      end if;
+  else
+      sel <= "00";
+  end if;
+  end process encoder;
 
-  acumulator : process(clk)
+  pc: process(pc_clk, sel)
+  begin
+    if rising_edge(pc_clk) then
+    case sel is
+      when "00" =>
+        q <= "00000000";
+      when "01" =>
+        -- count down
+        q <= q - 1;
+      -- when "10" =>; 
+        -- load
+      when "11" =>
+        -- count up
+        q <= q + 1;
+      when others =>
+    end case;
+  end if;
+  end process pc;
+
+  debugger: process(internal_clk, debug,step)
+  begin
+    pc_clk <= (internal_clk and not debug) or (step and internal_clk) or (step and internal_clk);
+  end process debugger;
+
+  acumulator : process(cp)
     begin
+      if rising_edge(cp) then
+        acc_data_in <= imm;
+        acc_data_out <= acc_data_in;
+        cp <= '0';
+      end if;
   end process acumulator;
 
   alu: process(alu_left,alu_right)
@@ -83,15 +141,15 @@ begin
 
     case alu_action is
       when ALU_OP_ADD =>
-          alu_left <= a;
+          alu_left <= acc_data_out;
           alu_right <= imm;
           output <= alu_left + alu_right;
       when ALU_OP_NAND =>
-          alu_left <= a;
+          alu_left <= acc_data_out;
           alu_right <= imm;
           output <= alu_left nand alu_right;
       when ALU_OP_XOR =>
-          alu_left <= a;
+          alu_left <= acc_data_out;
           alu_right <= imm;
           output <= alu_left xor alu_right;
       when ALU_OP_NOT =>
@@ -106,13 +164,13 @@ begin
     end case;
   end process alu;
 
-  decoder: process(operand) begin
+  -- d1-d6 are the same as alu_action
+  decoder: process(di) begin
 
-    case operand(2 downto 0) is
+    case di(2 downto 0) is
       -- LD
       when "000" =>
-        a <= operand;
-        alu_left <= a;
+        cp <= '1';
       -- ADD
       when "001" =>
         alu_action <= ALU_OP_ADD;
@@ -134,8 +192,12 @@ begin
       -- HALT
       when "111" =>
       -- HALT PC LOGIC
+        reset <= '1';
       when others =>
     end case;
 
   end process decoder;
+  di <= operand;
+  cp <= do(0);
+
 end cpu_arch;
