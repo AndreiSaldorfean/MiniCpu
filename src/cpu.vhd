391b85df162f521c2a3003fb59fef101c014aa19
Andrei Saldorfean <andreisaldorfean@gmail.com>
2025-01-14 01:32:11 +0200

    Improve logic and add better test

--- src/cpu.vhd
+++ src/cpu.vhd
@@ -3,76 +3,51 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity cpu_entity is Port(
-    byte_word  : in unsigned(7 downto 0); --instruction word
-    r_w    : in std_logic; --read/!write
-    load    : in std_logic;
-    cnt_down: in std_logic;
-    cnt_up  : in std_logic;
-    debug  : in std_logic;
-    step    : in std_logic;
-    output : out unsigned(3 downto 0)
+    byte_word    : in unsigned(7 downto 0);
+    alu_output   : out unsigned(4 downto 0);
+    r_w          : in std_logic;
+    load         : in std_logic;
+    cnt_down     : in std_logic;
+    cnt_up       : in std_logic;
+    debug        : in std_logic;
+    step         : in std_logic;
+    internal_clk : in std_logic
 );
 end cpu_entity;
 
 architecture cpu_arch of cpu_entity is
 
   -- Accumulator register
-  signal cp             : std_logic;
-  signal acc_data_in    : unsigned(4 downto 0);
-  signal acc_data_out   : unsigned(4 downto 0);
+  signal cp         : std_logic := '0';
+  signal acc_data   : unsigned(4 downto 0);
   -- ALU
-  type alu_action_t is
-  (
-    ALU_OP_ADD,
-    ALU_OP_NAND,
-    ALU_OP_XOR,
-    ALU_OP_NOT,
-    ALU_OP_LSH,
-    ALU_OP_RSH
-  );
-
-  type alu_src_t is
-  (
-    ALU_IMMEDIATE,
-    ALU_ACCUMULATOR
-  );
-
   signal alu_left   : unsigned(4 downto 0);
   signal alu_right  : unsigned(4 downto 0);
-  signal alu_action : alu_action_t;
-  -- Decoder
-  signal do : unsigned(7 downto 0);
-  signal di : unsigned(2 downto 0);
+  signal alu_action : unsigned(2 downto 0) := "000";
+
   -- PC
   signal sel : std_logic_vector(1 downto 0);
-  signal pc_clk : std_logic;
+  signal pc_clk : std_logic := '0';
   signal q : unsigned( 7 downto 0);
 
-  signal internal_clk : std_logic;
-  signal reset          : std_logic;
-  -- RAM 2K x 8 Static RAM
-  signal instruction_mode : unsigned(7 downto 0);
-  -- signal pc: unsigned(7 downto 0);
-  signal mem_addr       : unsigned(7 downto 0);
-  -- signal mem_data       : unsigned(7 downto 0);
-  type ram_type is array ((2**8)-1 to 0) of unsigned(7 downto 0);
-  signal mem_data       : ram_type;
-  signal operand        : unsigned(2 downto 0);
-  signal imm            : unsigned(4 downto 0);
+  signal reset          : std_logic := '0';
+  signal mem_addr       : unsigned(7 downto 0) := (others => '0');
+  signal mem_data       : unsigned(7 downto 0):= (others => '0');
+  signal operand        : unsigned(2 downto 0):= (others => '0');
+  signal imm            : unsigned(4 downto 0):= (others => '0');
 
 begin
 
   ram : process(q,debug,r_w)
     begin
-        mem_addr<=q;
-        if debug = '1' then
-          if not r_w ='1' then
-            mem_data(to_integer(mem_addr))<=byte_word;
-          end if;
-        end if;
-        operand <= mem_data(to_integer(mem_addr))(7 downto 5);
-        imm <= mem_data(to_integer(mem_addr))(4 downto 0);
-
+    mem_addr<=q;
+    if debug = '1' then
+      if not r_w ='1' then
+        mem_data<=byte_word;
+      end if;
+    end if;
+    operand <= byte_word(7 downto 5);
+    imm <= byte_word(4 downto 0);
   end process ram;
 
   encoder: process(cnt_down, cnt_up, load, reset)
@@ -119,86 +94,103 @@ begin
         -- count up
         q <= q + 1;
       when others =>
+        q <= "00000000";
     end case;
   end if;
   end process pc;
 
   debugger: process(internal_clk, debug,step)
   begin
-    pc_clk <= (internal_clk and not debug) or (step and internal_clk) or (step and internal_clk);
+    pc_clk <= (internal_clk and not debug) or (step and internal_clk) or (debug and step); -- NEEDS FIX
   end process debugger;
 
-  acumulator : process(cp)
-    begin
-      if rising_edge(cp) then
-        acc_data_in <= imm;
-        acc_data_out <= acc_data_in;
-        cp <= '0';
-      end if;
-  end process acumulator;
+  -- acumulator : process(cp)
+  --   begin
+  --       acc_data <= imm;
+  -- end process acumulator;
 
-  alu: process(alu_left,alu_right)
+  alu: process(alu_action)
     begin
 
     case alu_action is
-      when ALU_OP_ADD =>
-          alu_left <= acc_data_out;
+      when "001" =>
+          alu_left <= acc_data;
           alu_right <= imm;
-          output <= alu_left + alu_right;
-      when ALU_OP_NAND =>
-          alu_left <= acc_data_out;
+          alu_output <= alu_left + alu_right;
+      when "010" =>
+          alu_left <= acc_data;
           alu_right <= imm;
-          output <= alu_left nand alu_right;
-      when ALU_OP_XOR =>
-          alu_left <= acc_data_out;
+          alu_output <= alu_left nand alu_right;
+      when "011" =>
+          alu_left <= acc_data;
           alu_right <= imm;
-          output <= alu_left xor alu_right;
-      when ALU_OP_NOT =>
+          alu_output <= alu_left xor alu_right;
+      when "100" =>
           alu_right <= imm;
-          output <= not alu_right;
-      when ALU_OP_LSH =>
+          alu_output <= not alu_right;
+      when "101" =>
           alu_right <= imm;
-          output <= alu_right sll 1;
-      when ALU_OP_RSH =>
+          alu_output <= alu_right sll 1;
+      when "110" =>
           alu_right <= imm;
-          output <= alu_right srl 1;
+          alu_output <= alu_right srl 1;
+      when others =>
+          alu_left <= "00000";
+          alu_right <= "00000";
+          alu_output <= "00000";
     end case;
   end process alu;
 
-  -- d1-d6 are the same as alu_action
-  decoder: process(di) begin
+  decoder: process(operand,imm,debug) begin
 
-    case di(2 downto 0) is
-      -- LD
-      when "000" =>
-        cp <= '1';
-      -- ADD
-      when "001" =>
-        alu_action <= ALU_OP_ADD;
-      -- NAND
-      when "010" =>
-        alu_action <= ALU_OP_NAND;
-      -- XOR
-      when "011" =>
-        alu_action <= ALU_OP_XOR;
-      -- NOT
-      when "100" =>
-        alu_action <= ALU_OP_NOT;
-      -- LSH
-      when "101" =>
-        alu_action <= ALU_OP_LSH;
-      -- RSH
-      when "110" =>
-        alu_action <= ALU_OP_RSH;
-      -- HALT
-      when "111" =>
-      -- HALT PC LOGIC
-        reset <= '1';
-      when others =>
-    end case;
+    if debug = '0' then
+      case operand(2 downto 0) is
+        -- LD
+        when "000" =>
+          -- cp <= '1';
+          acc_data <= imm;
+          reset <= '0';
+        -- ADD
+        when "001" =>
+          alu_action <= "001";
+          reset <= '0';
+          cp <= '0';
+        -- NAND
+        when "010" =>
+          alu_action <= "010";
+          reset <= '0';
+          cp <= '0';
+        -- XOR
+        when "011" =>
+          alu_action <= "011";
+          reset <= '0';
+          cp <= '0';
+        -- NOT
+        when "100" =>
+          alu_action <= "100";
+          reset <= '0';
+          cp <= '0';
+        -- LSH
+        when "101" =>
+          alu_action <= "101";
+          reset <= '0';
+          cp <= '0';
+        -- RSH
+        when "110" =>
+          alu_action <= "110";
+          reset <= '0';
+          cp <= '0';
+        -- HALT
+        when "111" =>
+        -- HALT PC LOGIC
+          reset <= '1';
+          cp <= '0';
+        when others =>
+          reset <= '0';
+          cp <= '0';
+      end case;
+    end if;
 
   end process decoder;
-  di <= operand;
-  cp <= do(0);
 
 end cpu_arch;
--- src/cpu_tb.vhd
+++ src/cpu_tb.vhd
@@ -1,20 +1,96 @@
--- Test bench script
 library ieee;
-use ieee.std_logic_1164.all; --include arrays
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
-entity cpu_tb is Port(
-    Input : in std_logic_vector(7 downto 0);
-    r_w : in std_logic;
-    debug : in std_logic;
-    nxt : in std_logic;
-    prev : in std_logic;
-    clk : in std_logic;
-    output : out std_logic_vector( 3 downto 0)
-);
+entity cpu_tb is
 end cpu_tb;
 
-architecture cpu_arch_tb of cpu_tb is
+architecture testbench of cpu_tb is
+
+  -- Component declaration for the unit under test (UUT)
+  component cpu_entity
+    port (
+      byte_word  : in unsigned(7 downto 0); -- instruction word
+      r_w        : in std_logic;            -- read/!write
+      load       : in std_logic;
+      cnt_down   : in std_logic;
+      cnt_up     : in std_logic;
+      debug      : in std_logic;
+      step       : in std_logic;
+      alu_output     : out unsigned(4 downto 0);
+      internal_clk : in std_logic
+    );
+  end component;
+
+  -- Testbench signals
+  signal tb_byte_word : unsigned(7 downto 0) := (others => '0');
+  signal tb_r_w       : std_logic := '0';
+  signal tb_load      : std_logic := '0';
+  signal tb_cnt_down  : std_logic := '0';
+  signal tb_cnt_up    : std_logic := '0';
+  signal tb_debug     : std_logic := '0';
+  signal tb_step      : std_logic := '0';
+  signal tb_output    : unsigned(4 downto 0);
+  signal tb_internal_clk: std_logic := '0';
+
+  -- Clock generation
+  signal clk          : std_logic := '0';
 
 begin
 
-end cpu_arch_tb;
+
+  -- Instantiate the Unit Under Test (UUT)
+  uut: cpu_entity
+    port map (
+      byte_word  => tb_byte_word,
+      r_w        => tb_r_w,
+      load       => tb_load,
+      cnt_down   => tb_cnt_down,
+      cnt_up     => tb_cnt_up,
+      debug      => tb_debug,
+      step       => tb_step,
+      alu_output     => tb_output,
+      internal_clk => tb_internal_clk
+    );
+
+  -- Clock generation for 10 MHz
+  clk_process : process
+  begin
+    tb_internal_clk <= '0';
+    wait for 50 ns;
+    tb_internal_clk <= '1';
+    wait for 50 ns;
+  end process clk_process;
+
+  process
+  begin
+    -- Init memory
+    tb_debug <= '1';
+    tb_r_w <= '0';
+    tb_load <= '0';
+    tb_cnt_down <= '0';
+    tb_cnt_up <= '0';
+    tb_step <= '0';
+    wait for 10 ns;
+
+    tb_cnt_up <= '1';
+    wait for 10 ns;
+    tb_byte_word <= "00000101"; -- LD 5
+    wait for 10 ns;
+    tb_step <= '1';
+    wait for 10 ns;
+    tb_debug <= '0';
+    wait for 10 ns;
+    tb_step <= '0';
+    wait for 50 ns;
+    tb_debug <= '1';
+    wait for 10 ns;
+    tb_byte_word <= "00100010"; -- ADD 2
+    wait for 10 ns;
+    tb_step <= '1';
+    wait for 10 ns;
+    tb_debug <= '0';
+    report "Simulation complete" severity note;
+    wait;  -- Stop simulation
+  end process;
+end testbench;
